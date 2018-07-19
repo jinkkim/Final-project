0c0fdb491de718f46b5af94e851d7c70f77d5949
Jin <celine.jkim@gmail.com>
2018-07-19 02:19:39 -0400

    update

--- resources/db/models/AlbumDB.js
+++ resources/db/models/AlbumDB.js
@@ -1,19 +1,21 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var CalendarSchema = new Schema({
+var AlbumSchema = new Schema({
   couple_key: {
     type: String,
     required: true
   },
-  event: {
-    type: String,    
+  photo_link: {
+    type: String,
     required: true
   },
   date: {
-    type: Date,
-    required: true
+    type: Date
+  },
+  descr: {
+    type: String
   }
 });
 
-var Calendar = mongoose.model("Calendar", CalendarSchema);
-module.exports = Calendar;
+var Album = mongoose.model("Album", AlbumSchema);
+module.exports = Album;
